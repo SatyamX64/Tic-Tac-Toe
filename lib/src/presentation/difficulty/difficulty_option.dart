7208f85066214449c710052fa1bd38f330409a1e
satyam <satyamx64@gmail,com>
2022-04-27 03:03:12 +0530

    [UI] : added difficulty screen

---
+++ lib/src/presentation/difficulty/difficulty_option.dart
@@ -0,0 +1,68 @@
+part of 'sc_difficulty.dart';
+
+class _DifficultyOption extends StatelessWidget {
+  const _DifficultyOption({
+    required this.image,
+    required this.title,
+    required this.primary,
+    required this.secondary,
+    Key? key,
+  }) : super(key: key);
+
+  final ImageProvider image;
+  final String title;
+  final Color primary;
+  final Color secondary;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      width: double.maxFinite,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 68,
+              width: double.maxFinite,
+              margin: const EdgeInsets.symmetric(horizontal: 52),
+              decoration: BoxDecoration(
+                  color: secondary,
+                  borderRadius: BorderRadius.circular(72),
+                  border: Border.all(color: primary, width: 2)),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(72),
+                  ),
+                  padding: const EdgeInsets.only(left: 60),
+                ),
+                child: Text(
+                  title,
+                  style: GoogleFonts.poppins(
+                      fontSize: 20, fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.right,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 52,
+            child: Container(
+              decoration: BoxDecoration(shape: BoxShape.circle, color: primary),
+              height: 120,
+              width: 120,
+              padding: const EdgeInsets.all(24),
+              child: Image(
+                image: image,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/src/presentation/difficulty/sc_difficulty.dart
+++ lib/src/presentation/difficulty/sc_difficulty.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe/src/presentation/game/sc_game.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tic_tac_toe/gen/assets.gen.dart';
 import 'package:tic_tac_toe/src/presentation/shared/gradient_scaffold.dart';
+
+part 'difficulty_option.dart';
 
 class DifficultyScreen extends StatelessWidget {
   const DifficultyScreen({Key? key}) : super(key: key);
@@ -10,11 +13,59 @@ class DifficultyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GradientScaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pushReplacementNamed(GameScreen.route);
-          },
-          child: const Text('Select Difficulty'),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 60),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Select Difficulty',
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w700, fontSize: 32),
+                    ),
+                    const SizedBox(
+                      width: 174,
+                      child: Divider(
+                        color: Colors.amber,
+                        thickness: 4,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            _DifficultyOption(
+              secondary: const Color(0xFFE06EF9),
+              primary: const Color(0xFF5B4DD0),
+              title: 'Easy Bot',
+              image: Assets.images.easy,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            _DifficultyOption(
+              secondary: const Color(0xFFDE6536),
+              primary: const Color(0xFFFBDB45),
+              title: 'Medium Bot',
+              image: Assets.images.medium,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            _DifficultyOption(
+              secondary: const Color(0xFF3F88B1),
+              primary: const Color(0xFFE56871),
+              title: 'Difficult Bot',
+              image: Assets.images.hard,
+            ),
+            const Spacer(),
+          ],
         ),
       ),
     );
