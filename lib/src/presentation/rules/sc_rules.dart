69720d13522755deb9b6409ec1f6feccf4f8db1a
satyam <satyamx64@gmail,com>
2022-04-29 02:34:19 +0530

    [feat] : added animation to rules screen

--- lib/src/presentation/rules/sc_rules.dart
+++ lib/src/presentation/rules/sc_rules.dart
@@ -2,14 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/gen/assets.gen.dart';
 import 'package:tic_tac_toe/src/presentation/shared/gradient_scaffold.dart';
 
-class RulesScreen extends StatelessWidget {
+class RulesScreen extends StatefulWidget {
   const RulesScreen({Key? key}) : super(key: key);
 
   static const route = '/rules';
 
   static const _style = TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
+
+  @override
+  State<RulesScreen> createState() => _RulesScreenState();
+}
+
+class _RulesScreenState extends State<RulesScreen> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+
+  List<String> _items = [];
+
+  static const _source = [
+    '• You can only draw on empty tiles',
+    '• 3 match in a row/column  wins',
+    '• 3 match in a diagonal  wins',
+    '• You can never defeat the hard bot :)',
+    '• This list looks too short',
+    '• I should probably add more points',
+  ];
+  @override
+  void initState() {
+    super.initState();
+    addRules();
+  }
+
+  void addRules() async {
+    for (int i = 0; i < _source.length; i++) {
+      await Future.delayed(const Duration(milliseconds: 60));
+      addItem(i);
+    }
+  }
+
+  void addItem(int index) {
+    _listKey.currentState?.insertItem(_items.length,
+        duration: const Duration(milliseconds: 500));
+    _items = [
+      ..._items,
+      _source[index],
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget slideIt(BuildContext context, int index, animation) {
+      var item = _items[index];
+      return SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(1, 0),
+          end: const Offset(0, 0),
+        ).animate(animation),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: Text(
+            item,
+            style: RulesScreen._style,
+          ),
+        ),
+      );
+    }
+
     return GradientScaffold(
       body: SizedBox(
         width: double.maxFinite,
@@ -26,36 +83,14 @@ class RulesScreen extends StatelessWidget {
               height: 30,
             ),
             Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  '• You can only draw on empty tiles',
-                  style: _style,
-                ),
-                Text(
-                  '• 3 match in a row/column  wins',
-                  style: _style,
-                ),
-                Text(
-                  '• 3 match in a diagonal  wins',
-                  style: _style,
-                ),
-                Text(
-                  '• You can never defeat the hard bot :)',
-                  style: _style,
-                ),
-                Text(
-                  '• This list looks too short',
-                  style: _style,
-                ),
-                Text(
-                  '• I should probably add more points',
-                  style: _style,
-                ),
-              ],
-            )),
+              child: AnimatedList(
+                key: _listKey,
+                initialItemCount: 0,
+                itemBuilder: (context, index, animation) {
+                  return slideIt(context, index, animation);
+                },
+              ),
+            ),
             Assets.images.rule.image(),
           ],
         ),
